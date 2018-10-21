779742dada28d7fb24a4f6801634dd93f104139d
ford42 <ford@ringzero.de>
2018-10-21 11:53:07 +0200

    exception for url

--- CrawlerHeisse.py
+++ CrawlerHeisse.py
@@ -36,66 +36,74 @@ class ArticleFetcher:
 
     def fetch(self):
 
-        self.articles = []
-        # i = 0
+        try:
 
-        while True:
-            time.sleep(1)
+            self.articles = []
+            # i = 0
+
+            while True:
+                time.sleep(1)
+                print(self.url)
+                r = requests.get(self.url)
+                posts = BeautifulSoup(r.text, "html.parser")
+
+                for post in posts.select("article.news.row"):
+
+                    if post.select_one(".tp_title") is not None:
+                        title = post.select_one(".tp_title").text
+                    else:
+                        title = "Kein Title vorhanden!"
+                    print(title)
+
+                    if post.select_one("p") is not None:
+                        content = post.select_one("p").text
+                    else:
+                        content = "Kein Content vorhanden!"
+                    print(content)
+
+                    if post.select_one("img") is not None:
+                        sub_url = post.select_one("img").attrs["src"]
+                        image = urljoin(self.url, sub_url)
+                    else:
+                        image = "Kein Bild vorhanden!"
+                    print(image)
+
+                    if post.select_one("li.has-author") is not None:
+                        author = post.select_one("li.has-author").text
+                    else:
+                        author = "Kein Autor vorhanden!"
+                    print(author)
+
+                    if post.select_one("time") is not None:
+                        data = post.select_one("time").text
+                    else:
+                        data = "Kein Datum vorhanden!"
+                    print(data)
+
+                    if post.select_one("span.count.comment_count") is not None:
+                        count_comments = post.select_one("span.count.comment_count").text
+                    else:
+                        count_comments = "Keine Anzahl der Kommentare vorhanden!"
+                    print(count_comments)
+
+                    crawled = CrawledArticle(title, content, image, author, data, count_comments)
+                    self.articles.append(crawled)
+
+                    print("\n\n\n")
+                    # i += 1
+
+                    # print(i)
+
+                if not self.next_page(self.url):  # or i >= 42*3:
+                    break
+
+            return self.articles
+
+        except:
+
+            print('Die URL ist falsch!')
             print(self.url)
-            r = requests.get(self.url)
-            posts = BeautifulSoup(r.text, "html.parser")
-
-            for post in posts.select("article.news.row"):
-
-                if post.select_one(".tp_title") is not None:
-                    title = post.select_one(".tp_title").text
-                else:
-                    title = "Kein Title vorhanden!"
-                print(title)
-
-                if post.select_one("p") is not None:
-                    content = post.select_one("p").text
-                else:
-                    content = "Kein Content vorhanden!"
-                print(content)
-
-                if post.select_one("img") is not None:
-                    sub_url = post.select_one("img").attrs["src"]
-                    image = urljoin(self.url, sub_url)
-                else:
-                    image = "Kein Bild vorhanden!"
-                print(image)
-
-                if post.select_one("li.has-author") is not None:
-                    author = post.select_one("li.has-author").text
-                else:
-                    author = "Kein Autor vorhanden!"
-                print(author)
-
-                if post.select_one("time") is not None:
-                    data = post.select_one("time").text
-                else:
-                    data = "Kein Datum vorhanden!"
-                print(data)
-
-                if post.select_one("span.count.comment_count") is not None:
-                    count_comments = post.select_one("span.count.comment_count").text
-                else:
-                    count_comments = "Keine Anzahl der Kommentare vorhanden!"
-                print(count_comments)
-
-                crawled = CrawledArticle(title, content, image, author, data, count_comments)
-                self.articles.append(crawled)
-
-                print("\n\n\n")
-                # i += 1
-
-                # print(i)
-
-            if not self.next_page(self.url):  # or i >= 42*3:
-                break
-
-        return self.articles
+            quit()
 
 
 url = "https://www.heise.de/tp/energie-und-klima/?seite=333"
